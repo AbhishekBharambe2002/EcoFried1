8603634eb35f01fabbc78f1fdbd2ca7fbdaa903a
GoldenWind2002 <123597077+GoldenWind2002@users.noreply.github.com>
2023-10-10 14:31:57 +0530

    Sample Commit

---
+++ Backend/data.js
@@ -0,0 +1,32 @@
+const data = {
+    products:[
+        {
+            name: 'Ganesh Statue1',
+            slug:'ganesh statue1',
+            category:'statues',
+            image:'/images/p1.jpg',
+            price:'120',
+            countInStock:'10',
+            description:'clay statue',
+        },
+        {
+            name: 'Ganesh Statue2',
+            slug:'ganesh statue2',
+            category:'statues',
+            image:'/images/p2.jpg',
+            price:'110',
+            countInStock:'10',
+            description:'clay statue',
+        },
+        {
+            name: 'Ganesh Statue3',
+            slug:'ganesh statue3',
+            category:'statues',
+            image:'/images/p3.jpg',
+            price:'130',
+            countInStock:'10',
+            description:'clay statue',
+        },
+    ],
+}
+export default data;
